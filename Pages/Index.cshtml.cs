633a190393c5fd5d07708e9fce13633c30a74519
vragnaroda <kirill.ste.2016@gmail.com>
2026-08-30 01:10:26 +0300

    Рабочая форма авторизации + список пользователей подтягивается

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,18 +1,33 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using webasp.Data;
+using webasp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace webasp.Pages
 {
     [Authorize]
     public class IndexModel : PageModel
     {
-        
-        public void OnGet()
+        private readonly DB _db;
+
+        public List<User> UsersList { get; set; } = new();
+
+        public IndexModel(DB db)
         {
+            _db = db;
         }
+
+        public async Task<IActionResult> OnGet()
+        {
+            UsersList = await _db.Users.ToListAsync();
+            return Page();
+        }
+
 
 
         public async Task<IActionResult> OnPostAsync()
